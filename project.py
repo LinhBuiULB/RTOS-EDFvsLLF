88d3269a6438268b852bb9b803b6ca787a5028ba
Linh Bui Quang Phuong <linh.a.bui@gmail.com>
2018-12-12 23:00:12 +0100

    refactor+comms

--- project.py
+++ project.py
@@ -11,14 +11,9 @@ from matplotlib.legend_handler import HandlerPatch
 
 UPPER_BOUND_VALUE = 9999
 
-def LCM(numbers):
-	"""
-	Computing least common multiple
-	"""
-	lcm = numbers[0]
-	for i in numbers[1:]:
-	  lcm = lcm*i//gcd(lcm, i)
-	return lcm
+##################################################
+#               Parsing functions                #
+##################################################
 
 def readFile(filename):
 	"""
@@ -46,21 +41,42 @@ def getOffsetWCETPeriodLists(systemList):
 
 	return offsetList, wcetList, periodList 
 
-def computeFeasibilityInterval(newSystemList):
-    """
-    Printing the feasibility interval of a list of offset, WCET and periods values
-    from a system.txt file
-    """ 
-    offsetList, wcetList, periodList = getOffsetWCETPeriodLists(newSystemList)
-    feasibilityIntervalUpperBound = max(offsetList) + (2*LCM(periodList))
 
-    return feasibilityIntervalUpperBound
+
+##################################################
+#     EDF feasibility interval computation       #
+##################################################
+
+def LCM(numbers):
+	"""
+	Computing least common multiple
+	"""
+	lcm = numbers[0]
+	for i in numbers[1:]:
+	  lcm = lcm*i//gcd(lcm, i)
+	return lcm
+
+def computeFeasibilityInterval(newSystemList):
+	"""
+	Printing the feasibility interval of a list of offset, WCET and periods values
+	from a system.txt file
+	""" 
+	offsetList, wcetList, periodList = getOffsetWCETPeriodLists(newSystemList)
+	feasibilityIntervalUpperBound = max(offsetList) + (2*LCM(periodList))
+
+	return feasibilityIntervalUpperBound
 
 def printFeasibilityInterval(feasibilityIntervalUpperBound):
 	"""
 	Print the feasibility interval (question 1)
 	"""
-	print("feasibility interval: 0 ," ,feasibilityIntervalUpperBound)
+	print("0,{}".format(feasibilityIntervalUpperBound))
+
+
+
+##################################################
+#                System generator                #
+##################################################
 
 def matchRequiredUtilisationPercentage(wcets, periods, percentage, delta):
 	"""
@@ -100,9 +116,7 @@ def generateTasks(numberOfTasks, requiredUtilisationPercentage, delta):
 						break
 			if matchRequiredUtilisationPercentage(wcets, periods, requiredUtilisationPercentage, delta):
 				loop = False
-	print(offsets, wcets, periods)
 	return offsets, wcets, periods
-
 
 def systemFileGenerator(offsets, wcets, periods,filename):
 	"""
@@ -113,6 +127,12 @@ def systemFileGenerator(offsets, wcets, periods,filename):
 		file.write(str(offsets[i]) + "; " + str(wcets[i]) + "; " + str(periods[i]) + "\n")
 	file.close()
 	print("Your file {} has been succesfully generated".format(filename))
+
+
+
+##################################################
+#           EDF and LLF implementation           #
+##################################################
 
 def getMultiplesOf(number, limit, offset):
 	"""
@@ -160,7 +180,6 @@ def isDeadlineMissed(deadline, t):
 	"""
 	return deadline <= t
 
-
 def initJobsList(systemList):
 	"""
 	Initialize the list of jobs's tasks to 0 
@@ -198,79 +217,16 @@ def computeLaxities(time, tasksDeadlines, e, isJobDoneUntilNextDeadline, CPUTime
 
 	return newLaxities
 
-
-def LLF(system, begin, end):
-	"""
-	LLF schedule
-	"""
-	systemList = readFile(system)
-	jobs = initJobsList(systemList)
-	offsetList, wcetList, periodList = getOffsetWCETPeriodLists(systemList)
-	print("offsets",offsetList)
-	tasksDeadlinesDict = getTasksDeadlines(systemList, computeFeasibilityInterval(systemList), offsetList)
-	tasksExecuted = []
-	isJobDoneUntilNextDeadline = initIsJobDoneDict(systemList) # dict that allows us to get if the jobs are done for the currrent deadline 
-	arrivalJob = copy.deepcopy(tasksDeadlinesDict)
-	arrivalJobOutput = []
-	preemptionsNb = 0
-
-	laxityOfJobs = []
-	CPUTimeUsed = [0 for i in range(len(jobs))]
-
-	if(isSchedulable):
-		t = 0
-		wcets = copy.deepcopy(wcetList)
-
-		while(t <= end):
-
-			for deadlineList in arrivalJob.values():
-				for deadline in deadlineList: 
-					if(t == deadline):
-						isJobDoneUntilNextDeadline[list(arrivalJob.keys())[list(arrivalJob.values()).index(deadlineList)]] = False
-						arrivalJobOutput.append("{}:T{}J{}".format(t, list(arrivalJob.keys())[list(arrivalJob.values()).index(deadlineList)] , deadlineList.index(deadline) + 1 ))
-
-
-			laxityOfJobs = computeLaxities(t, tasksDeadlinesDict, wcetList, isJobDoneUntilNextDeadline, CPUTimeUsed)
-			print("Laxities are {}".format(laxityOfJobs))
-
-			currentExecutedTask = laxityOfJobs.index(min(laxityOfJobs))
-
-			if (t != 0):
-				if tasksExecuted[t-1][0] != currentExecutedTask:
-					if not isJobDoneUntilNextDeadline[tasksExecuted[t-1][0]]:
-						preemptionsNb += 1 
-
-			CPUTimeUsed[currentExecutedTask] += 1
-			wcets[currentExecutedTask] -= 1 
-			tasksExecuted.append((currentExecutedTask, jobs[currentExecutedTask]))
-
-			if(min(laxityOfJobs) < 0):
-				print("Missed")
-				tasksExecuted.append("Missed")
-				break
-
-			if(wcets[currentExecutedTask] == 0):
-				isJobDoneUntilNextDeadline[currentExecutedTask] = True
-				jobs[currentExecutedTask] += 1
-				wcets[currentExecutedTask] = wcetList[currentExecutedTask]
-				tasksDeadlinesDict[currentExecutedTask] = tasksDeadlinesDict[currentExecutedTask][1:] 
-				CPUTimeUsed[currentExecutedTask] = 0
-
-			print("Task executed are {} at time {} \n".format(tasksExecuted, t))
-
-			t += 1
-
-		printOutputs(tasksExecuted, arrivalJobOutput, begin, end, systemList, preemptionsNb)
-		printGraph(tasksExecuted,arrivalJob, offsetList, begin)
-
-
 def EDF(system, begin, end):
 	"""
-	EDF schedule
+	EDF scheduling
 	"""
+	# Parse the system file 
 	systemList = readFile(system)
-	jobs = initJobsList(systemList)
 	offsetList, wcetList, periodList = getOffsetWCETPeriodLists(systemList)
+
+	# Initialization of variables 
+	jobs = initJobsList(systemList)
 	tasksDeadlinesDict = getTasksDeadlines(systemList, computeFeasibilityInterval(systemList), offsetList)
 	tasksExecuted = []
 	isJobDoneUntilNextDeadline = initIsJobDoneDict(systemList) # dict that allows us to get if the jobs are done for the current deadline 
@@ -280,22 +236,26 @@ def EDF(system, begin, end):
 
 	if(isSchedulable):
 		t = 0
-		wcets = copy.deepcopy(wcetList)
+		wcets = copy.deepcopy(wcetList) # This copy allows us to know when a job is finished (when WCET of the task = 0)
 
 		while(t <= end):
 
+			# Arrivals managament
 			for deadlineList in arrivalJob.values():
 				for deadline in deadlineList: 
 					if(t == deadline):
 						isJobDoneUntilNextDeadline[list(arrivalJob.keys())[list(arrivalJob.values()).index(deadlineList)]] = False
 						arrivalJobOutput.append("{}:T{}J{}".format(t, list(arrivalJob.keys())[list(arrivalJob.values()).index(deadlineList)] , deadlineList.index(deadline) + 1 ))
 
+			# Assign the current executing task 
 			currentExecutedTask, smallest = getSmallestDeadlines(tasksDeadlinesDict, isJobDoneUntilNextDeadline)
 
+			# Check if the deadline is missed 
 			if isDeadlineMissed(smallest, t):
 				tasksExecuted.append("Missed")
 				break
 
+			# Append the current executing task to the tasksExecuted and update wcets
 			tasksExecuted.append((currentExecutedTask, jobs[currentExecutedTask]))
 			wcets[currentExecutedTask] -= 1 
 
@@ -314,36 +274,119 @@ def EDF(system, begin, end):
 
 			t += 1
 
+		# Printing outputs in terminal and graphically 
 		printOutputs(tasksExecuted, arrivalJobOutput, begin, end, systemList, preemptionsNb)
 		printGraph(tasksExecuted, arrivalJob, offsetList, begin)
+
+def LLF(system, begin, end):
+	"""
+	LLF scheduling
+	"""
+	# Parse the system file 
+	systemList = readFile(system)
+	offsetList, wcetList, periodList = getOffsetWCETPeriodLists(systemList)
+
+	# Initialization of variables
+	jobs = initJobsList(systemList)
+	tasksDeadlinesDict = getTasksDeadlines(systemList, computeFeasibilityInterval(systemList), offsetList)
+	tasksExecuted = []
+	isJobDoneUntilNextDeadline = initIsJobDoneDict(systemList) # dict that allows us to get if the jobs are done for the current deadline 
+	arrivalJob = copy.deepcopy(tasksDeadlinesDict)
+	arrivalJobOutput = []
+	preemptionsNb = 0
+
+	# Laxity initialization
+	laxityOfJobs = []
+	CPUTimeUsed = [0 for i in range(len(jobs))]
+
+	if(isSchedulable):
+		t = 0
+		wcets = copy.deepcopy(wcetList) # This copy allows us to know when a job is finished (when WCET of the task = 0)
+
+		while(t <= end):
+
+			# Arrivals managament
+			for deadlineList in arrivalJob.values():
+				for deadline in deadlineList: 
+					if(t == deadline):
+						isJobDoneUntilNextDeadline[list(arrivalJob.keys())[list(arrivalJob.values()).index(deadlineList)]] = False
+						arrivalJobOutput.append("{}:T{}J{}".format(t, list(arrivalJob.keys())[list(arrivalJob.values()).index(deadlineList)] , deadlineList.index(deadline) + 1 ))
+
+			# Computing of the laxities for every job 
+			laxityOfJobs = computeLaxities(t, tasksDeadlinesDict, wcetList, isJobDoneUntilNextDeadline, CPUTimeUsed)
+
+			# Assign the current executing task
+			currentExecutedTask = laxityOfJobs.index(min(laxityOfJobs))
+
+			# Compute preemption number
+			if (t != 0):
+				if tasksExecuted[t-1][0] != currentExecutedTask:
+					if not isJobDoneUntilNextDeadline[tasksExecuted[t-1][0]]:
+						preemptionsNb += 1 
+
+			# Append the current executing task to the tasksExecuted and update wcets and CPUTimeUsed
+			CPUTimeUsed[currentExecutedTask] += 1
+			wcets[currentExecutedTask] -= 1 
+			tasksExecuted.append((currentExecutedTask, jobs[currentExecutedTask]))
+
+			# Check if the deadline is missed
+			if(min(laxityOfJobs) < 0):
+				tasksExecuted.append("Missed")
+				break
+
+			# If job done 
+			if(wcets[currentExecutedTask] == 0):
+				isJobDoneUntilNextDeadline[currentExecutedTask] = True
+				jobs[currentExecutedTask] += 1
+				wcets[currentExecutedTask] = wcetList[currentExecutedTask]
+				tasksDeadlinesDict[currentExecutedTask] = tasksDeadlinesDict[currentExecutedTask][1:] 
+				CPUTimeUsed[currentExecutedTask] = 0
+
+			t += 1
+
+		# Printing outputs in terminal and graphically
+		printOutputs(tasksExecuted, arrivalJobOutput, begin, end, systemList, preemptionsNb)
+		printGraph(tasksExecuted,arrivalJob, offsetList, begin)
 
 def printOutputs(tasksExecuted, arrivalJobOutput, begin, end, systemList, preemptionsNb):
 	"""
 	Print the schedule of the system when the schedule is over
 	"""
 	print("Schedule from", begin, "to", end, ";", len(systemList), "tasks")
+
 	i=0
 	while(i < len(tasksExecuted)-1):
 		interval = getTaskInterval(tasksExecuted, tasksExecuted[i][0], i)
+
+		# Printing the executing jobs 
 		if(i >= begin):
 			print("{}-{} : T{}J{}".format(i, i+interval, tasksExecuted[i][0], tasksExecuted[i][1]))
 
+		# Printing the arrival of jobs 
 		for j in range(i+1, i+interval+1):
 			for elem in arrivalJobOutput:
 				if int(elem.split(":")[0]) == j:
 					if(tasksExecuted[j] != "Missed"):
 						print("{} : Arrival of job {}".format(j, elem.split(":")[1]))
 		i+=interval
+
+		# Stop printing if deadline missed 
 		if (tasksExecuted[i] == "Missed"):
 			print("{}: Job T{}J{} misses a deadline".format(i, tasksExecuted[-2][0], tasksExecuted[-2][1]))
 			print("END: {} preemptions".format(preemptionsNb))
 			break
 
 def make_legend_arrow(legend, orig_handle,xdescent, ydescent,width, height, fontsize):
-    p = mpatches.FancyArrow(0, 0.5*height, width, 0, length_includes_head=True, head_width=0.75*height )
-    return p
+	"""
+	Manage the arrow for the legend of the GUI 
+	"""
+	p = mpatches.FancyArrow(0, 0.5*height, width, 0, length_includes_head=True, head_width=0.75*height)
+	return p
 
 def printGraph(tasksExecuted, arrivalJob, offsetList, begin):
+	"""
+	Plotting the schedule graphically 
+	"""
 
 	x = []
 	y = []
@@ -351,9 +394,7 @@ def printGraph(tasksExecuted, arrivalJob, offsetList, begin):
 	yCircle = [] 
 	plt.figure(figsize=(15,13)) 
 	
-	"""
-	create two arrays for the execution time to be plotted
-	"""
+	# Create two arrays for the execution time to be plotted
 	missed = 0
 	for i in range(begin, len(tasksExecuted)):
 		if(tasksExecuted[i] != "Missed"):
@@ -364,19 +405,14 @@ def printGraph(tasksExecuted, arrivalJob, offsetList, begin):
 			missed += 1
 			plt.text(i,tasksExecuted[i-1][0], "Job missed !",ha="center", va="center",bbox=dict(boxstyle="round",ec=(1., 0.5, 0.5),fc=(1., 0.8, 0.8),))
 
-
-	"""
-	create two arrays for the deadlines to be plotted and draw the arrows corresponding to the arrival
-	"""
+	# Create two arrays for the deadlines to be plotted and draw the arrows corresponding to the arrival
 	for i in range(len(arrivalJob)):
 		for deadline in arrivalJob[i]:
 			yCircle.append(i)
 			xCircle.append(deadline)
 			jobArrival  = plt.arrow(deadline,i-0.35,0,0.2, fc="k", ec="k", head_width = 0.5, head_length = 0.1)
 
-	"""
-	draw the first arrival of the job of each tasks
-	"""
+	# Draw the first arrival of the job of each tasks
 	for i in range(len(offsetList)):
 		firstJob =plt.arrow(offsetList[i]+0.05,i-0.35,0,0.2,fc="g", ec="g", head_width = 0.5, head_length = 0.1, linewidth = 2)
 
@@ -402,6 +438,7 @@ def printGraph(tasksExecuted, arrivalJob, offsetList, begin):
 		countY += 1
 
 	executionTime = plt.barh(y, [1]*len(x), left=x, color = 'blue', edgecolor = 'green', align='center', height=0.1, zorder = -1)
+
 	plt.grid(color='black', linestyle='dotted', linewidth=1)
 	plt.ylim(max(y)+0.5, min(y)-0.5)
 	plt.xlim(min(x), max(x)+missed)
@@ -427,10 +464,11 @@ def getTaskInterval(tasksExecuted, task, index):
 	return interval
 
 def main(filename, scheduler, start, end):
+	"""
+	Main function excuting EDF or LLF 
+	"""
 	newSystemList = readFile(filename)
 
-	# Testing tasks generator 
-	print("\n# QUESTION 3")
 	if(scheduler == "edf"):
 		EDF(filename, start, end)
 	else:
@@ -438,6 +476,9 @@ def main(filename, scheduler, start, end):
 
 if __name__ == "__main__":
 
+	# Managing all arguments exceptions 
+
+	# Manage question 1 arguments: printing of the feasibility interval
 	if(len(sys.argv) == 3):
 		intervalType = sys.argv[1]
 		filename = sys.argv[2]
@@ -447,14 +488,17 @@ if __name__ == "__main__":
 				feasibilityIntervalUpperBound = computeFeasibilityInterval(newSystemList)
 				printFeasibilityInterval(feasibilityIntervalUpperBound)
 			except IOError  as e:
-				print("can not open {}".format(filename))
+				print("Cannot open {}".format(filename))
 		else:
 			print("Error : argument(s) incorrect.")
+
 	elif(len(sys.argv) == 5):
 		firstArg = sys.argv[1]
 		secondArg = sys.argv[2]
 		thirdArg = sys.argv[3]
 		fourthArg = sys.argv[4]
+
+		# Manage question 2 arguments : generator of tasks 
 		if(firstArg == "gen"):
 			try:
 				numberOfTasks = int(secondArg)
@@ -465,6 +509,7 @@ if __name__ == "__main__":
 			except ValueError as e:
 				print("Error : invalid value for the number of tasks or the pourcentage utilisation")
 		
+		# Manage question 3 arguments : EDF / LLF scheduling
 		elif(firstArg == "edf" or firstArg == "llf"):
 			try:
 				scheduler = sys.argv[1]
@@ -482,6 +527,7 @@ if __name__ == "__main__":
 				print("Error : invalid value for the bounds")
 		else:
 			print("Error : wrong arguments")
+
 	else:
 		if(len(sys.argv) > 5 ):
 			print("Error : too much arguments")
